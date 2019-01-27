42d1cbf936de7bb812ddee8dd8c2175e09da44a6
buzz1274 <david@sulaco.co.uk>
2019-01-27 11:53:36 +0000

    projects unit tests

--- tests/test_projects.py
+++ tests/test_projects.py
@@ -55,6 +55,20 @@ class TestProjects(object):
                     projects[2] == 'project_2' and projects[3] == 'project_3' and
                     projects[4] == 'project_4')
 
+    def test_sub_projects_are_sorted_alphabeticaly(self):
+        with patch.object(Tasks, 'search',
+                          return_value=Helper.load_fixture('projects')):
+            tasks = Tasks()
+
+            assert 'project_1' in tasks.projects.projects
+
+            sub_projects = \
+                list(tasks.projects.projects['project_1'].projects.projects)
+
+            assert (sub_projects[0] == 'project_1.sub_project_1' and
+                    sub_projects[1] == 'project_1.sub_project_2' and
+                    sub_projects[2] == 'project_1.sub_project_3')
+
     def test_calling_increment_task_count_increases_tasks_count_by_one(self):
         with patch.object(Tasks, 'search',
                           return_value=Helper.load_fixture('projects')):
@@ -78,6 +92,40 @@ class TestProjects(object):
             tasks.projects.projects['inbox'].increment_task_count(5)
 
             assert tasks.projects.projects['inbox'].task_count == 6
+
+    def test_if_inbox_project_is_used_ensure_correct_task_count_for_project(self):
+        with patch.object(Tasks, 'search',
+                          return_value=Helper.load_fixture('projects_inbox')):
+
+            tasks = Tasks()
+
+            assert tasks.projects.projects['inbox'].task_count == 3
+
+    def test_project_id_returns_project_if_no_parent(self):
+        projects = Projects()
+
+        assert projects.get_project_id('project_a') == 'project_a'
+
+    def test_project_id_returns_parent_followed_by_project_if_parent_exists(self):
+        projects = Projects('project_a')
+
+        assert projects.get_project_id('project_b') == 'project_a.project_b'
+
+    def test_add_will_add_a_new_project_to_collections(self):
+        projects = Projects()
+        projects.add('project_a', 'project_a', 5)
+
+        assert 'project_a' in projects.projects
+        assert projects.projects['project_a'].task_count == 5
+
+    def test_add_setting_last_to_false_will_add_a_project_in_first_place(self):
+        projects = Projects()
+        projects.add('project_a', 'project_a', 5)
+        projects.add('project_b', 'project_b', 2, False)
+
+        projects = list(projects.projects)
+
+        assert projects[0] == 'project_b'
 
 
 
--- tests/test_response.py
+++ tests/test_response.py
@@ -1,4 +1,6 @@
 import pytest
+from unittest.mock import patch
+from task_warrior.tasks import Tasks
 
 import tasks
 
@@ -12,7 +14,10 @@ class TestTasks(object):
         yield(client)
 
     def test_index_returns_a_200_status_code(self, client):
-        response = client.get('/')
+        with patch.object(Tasks, 'search', return_value='') as mocked_search:
+            response = client.get('/')
 
-        assert response.status_code == 200
-        assert b'<title>tasks.zz50.co.uk</title>' in response.data
+            mocked_search.assert_called_once()
+
+            assert response.status_code == 200
+            assert b'<title>tasks.zz50.co.uk</title>' in response.data
