c4e6b5bd38393af026edb578b536672b66344f80
buzz1274 <david@sulaco.co.uk>
2019-02-27 22:48:44 +0000

    start flask server in dev mode

--- flask.sh
+++ flask.sh
@@ -3,4 +3,9 @@
 cd /var/www/tasks.zz50.co.uk
 export FLASK_ENV=development
 
-flask run --host=0.0.0.0 --port=80
+if [[ $1 = 'dev' ]]; then
+    export FLASK_APP=tasks.py
+    flask run --host=0.0.0.0 --port=5000
+else
+    flask run --host=0.0.0.0 --port=80
+fi
